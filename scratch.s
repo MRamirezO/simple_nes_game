698d2591e0a718a1b04917f11f7f9e1fca873c3d
mramirezo <jmramirezobeso@gmail.com>
2024-09-06 15:54:26 -0700

    Added comments on OAM DMA transfer

--- scratch.s
+++ scratch.s
@@ -183,7 +183,7 @@ gameovertext:
 	bit PPU_STATUS
 	; transfer sprite OAM data using DMA
 	lda #>oam ; Loads highbyte of oam RAM address to A register
-	sta SPRITE_DMA ; Stores highbyte ($XX) of oam into DMA, which transfert 256bytes to VRAM ($XX00 - $XXFF)
+	sta SPRITE_DMA ; Stores highbyte ($XX) of oam into DMA, which transfert 256bytes from RAM OAM to PPU OAM ($XX00 - $XXFF)
 
 	; transfer current palette to PPU
 	vram_set_address $3F00
